7c17a9ff6ea5666a62ce7f07c74588b304ec729a
Nikolay <areyfclm@gmail.com>
2023-03-14 01:12:32 +0300

    developing history mode

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
 const btn = document.querySelector('.roll-btn')
 const reset = document.querySelector('.reset-btn')
+let history = document.querySelector('.history')
 let result = document.querySelector('p')
+
+let historyArr = []
 
 let resultBtn = btn.addEventListener('click', () => {
     let btnResult = Math.floor(Math.random() * 13)
 
     if (btnResult <= 1) {
-        Math.floor(Math.random() * 13)
+        Math.floor(Math.random())
     } else {
-        return result.innerHTML = btnResult
+        result.innerHTML = btnResult
+        historyArr.push(btnResult)
+        let res = historyArr.reduce((acc, number) => number + acc, 0)
+
+        if (historyArr.length > 5) {
+            historyArr.length = 0
+            res = 0
+        } else if(historyArr.length === 5) {
+            let fin = document.createElement('div')
+            fin.className = 'final-result'
+            fin.innerHTML = `Your result is ${res}`
+            history.appendChild(fin)
+        } 
+
+        console.log(btnResult);
     }
 })
 
 reset.addEventListener('click', () => {
-    return result.innerHTML = 0
+    result.innerHTML = 0
+    historyArr.length = 0
+    history.innerHTML = historyArr
 })
